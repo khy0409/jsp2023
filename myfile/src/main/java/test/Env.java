df38c0d9a9bc9bea8cbf2a3bbebea1c8bcd4a692
khy0409 <hyeyeong.ijn@gamil.com>
2023-11-15 15:01:16 +0900

    mvcvoard list

--- myfile/src/main/java/test/Env.java
+++ myfile/src/main/java/test/Env.java
@@ -1,17 +1,31 @@
 package test;
 
+import java.util.Map;
 import java.util.Properties;
 
 public class Env {
 
         public static void main(String[] args) {
+                printProps();
                 printEnv();
-                System.out.println(System.getProperty("user.dir"));
-                System.out.println(System.getProperty("sonar.token"));
-                System.out.println("---");
+                printEtc();
         }
 
-        public static void printEnv() {
+		public static void printEtc() {
+			System.out.println(System.getProperty("user.dir"));
+			System.out.println(System.getProperty("sonar.token"));
+			System.out.println(System.getenv("SONAR_TOKEN"));
+			System.out.println("---");
+		}
+
+        private static void printEnv() {
+                Map<String, String> map = System.getenv();
+                for (Map.Entry<String, String> entry: map.entrySet()) {
+                        System.out.println(entry.getKey() + ": " + entry.getValue());
+                }
+        }
+
+        public static void printProps() {
                 Properties properties = System.getProperties();
                 for (String key : properties.stringPropertyNames()) {
                         String value = properties.getProperty(key, "");
